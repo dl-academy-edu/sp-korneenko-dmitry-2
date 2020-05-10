31e0860291d4d17f910cdbc6a322f31a8f3d73ca
Dmitriy <ggbox@mail.ru>
2020-05-10 15:01:15 +0400

    js esc

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -12,17 +12,18 @@ function closeModal(){
     modalWindow.classList.add('hidden')
 }
 
-modalButton.addEventListener ('click', function (evt) {
+modalButton.addEventListener('click', function (evt) {
     evt.preventDefault(); showModal();
  });
 
 closeModalButton.addEventListener ('click', function (evt) {
-    evt.preventDefault(); showModal();
+    evt.preventDefault(); closeModal();
  });
 
 
  document.addEventListener('keydown', function (evt) {
-    if(e.keyCode === 27) closeModal();
+    if(e.keyCode === 27) {
+        closeModal()}
   }); 
 
 
@@ -30,15 +31,24 @@ closeModalButton.addEventListener ('click', function (evt) {
 
 let headerMenuButton = document.querySelector('.header__menu-link_js');
 let headerMenuModal = document.querySelector('.header__modal-menu_js');
+let headerMenuCloseButton = document.querySelector('.header__close-modal_js')
 
 function showHeaderMenu(){
     headerMenuModal.classList.remove('hidden')
 };
 
-function closeModal(){
-    modalWindow.classList.add('hidden')
+function closeHeaderModal(){
+    headerMenuModal.classList.add('hidden')
 };
 
-headerMenuButton.addEventListener ('click', function (evt) {
+
+headerMenuButton.addEventListener('click', function (evt) {
     evt.preventDefault(); showHeaderMenu();
  });
+
+headerMenuCloseButton.addEventListener('click', function (evt) {
+    evt.preventDefault(); closeHeaderModal();
+})
+
+
+
